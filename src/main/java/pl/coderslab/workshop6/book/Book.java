23604a3cbbf8b7535094e96d5507d719eac172ea
v-dmytriv <vdmytriv@visa.com>
2025-03-28 10:48:35 +0100

    Added Book Entity

---
+++ src/main/java/pl/coderslab/workshop6/book/Book.java
@@ -0,0 +1,23 @@
+package pl.coderslab.workshop6.book;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String type;
+}
